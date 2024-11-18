2d1689b70d7a10679f1db0b232513a1041592df4
Bhargav <bhargavhh0@gmail.com>
2024-11-18 18:15:02 +0530

    the prime Numbers

---
+++ app.js
@@ -0,0 +1,11 @@
+const numbers = [
+    19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+  ];
+  let primeArray = ''; 
+  for(let i =0; i<numbers.length;i++){
+    if(numbers[i]%2=== 1){
+       primeArray += numbers[i] + '';
+    }
+  }
+  console.log(primeArray);
+   
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const numbers = [
     3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
   ];
-  Let evenNumbers = '';
-  Let PrimeNumbers = '';
+  let evenNumbers = '';
+  let PrimeNumbers = '';
   for(let i = 0; i<numbers.length;i++){
     if(numbers[i]%2===0){
         evenNumbers += numbers[i] + ' '
